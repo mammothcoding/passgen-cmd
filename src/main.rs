109b6219f63ffd63a9b619670da1f36884c01e39
Mamontov <mammmoth@list.ru>
2024-12-06 01:59:03 +0300

    gen logic is done

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use clap_derive::Parser;
 use passgenlib::Passgen;
 
 #[derive(Parser, Debug)]
-#[command(version, verbatim_doc_comment, about = "\n\n⚙️ Cross-platform tool for generating cryptographically secure passwords/tokens and other sets and sequences.")]
+#[command(
+    version,
+    verbatim_doc_comment,
+    about = "\n\n⚙️ Cross-platform tool for generating cryptographically secure passwords/tokens and other sets and sequences."
+)]
 struct Args {
     /// Length of result.
     #[arg(default_value_t = 8)]
@@ -15,15 +19,15 @@ struct Args {
 
     /// Include capital letters to the generator.
     #[arg(short = 'L', long, default_value = "false")]
-    pub enab_u_letters: bool,
+    pub u_letters_on: bool,
 
     /// Include numeric characters to the generator.
     #[arg(short = 'n', long, default_value = "false")]
-    pub enab_num: bool,
+    pub nums_on: bool,
 
     /// Include special characters to the generator.
     #[arg(short = 's', long, default_value = "false")]
-    pub enab_spec_symbs: bool,
+    pub spec_symbs_on: bool,
 
     /// Including all characters, but
     /// the first position in the password is a capital or small letter,
@@ -31,28 +35,38 @@ struct Args {
     /// ⚠️If this rule is enabled, the other consistency rules of the generating are not taken,
     /// except for a rule "custom_charset".
     #[arg(short = 'S', long, default_value = "false", verbatim_doc_comment)]
-    pub enab_strong_usab: bool,
+    pub strong_usab_on: bool,
 
     /// User defined character set.
-    /// ⚠️This set of characters will exclude all other rules.
+    /// ⚠️This set of characters will exclude all other rules except for a rule "strong_usab_on".
+    /// ⚙️If "strong_usab_on" on too then you can generate combined strong and usability result with custom charset.
     #[arg(short, long, default_value = "", verbatim_doc_comment)]
     custom_charset: String,
 }
 
 fn main() {
-    let args = Args::parse();
+    let args: Args = Args::parse();
 
-    if args.custom_charset == "" {
-        println!(
-            "{}",
-            Passgen::default_strong_and_usab().generate(args.length)
-        );
+    fn is_ruleset_clean(args: &Args) -> bool {
+        args.custom_charset == ""
+            && !args.letters_on
+            && !args.u_letters_on
+            && !args.nums_on
+            && !args.spec_symbs_on
+            && !args.strong_usab_on
+    }
+
+    if is_ruleset_clean(&args) {
+        println!("{}", Passgen::default().generate(args.length));
     } else {
-        println!(
-            "{}",
-            Passgen::new()
-                .set_custom_charset(args.custom_charset.leak())
-                .generate(args.length)
-        );
+        let mut pgen = Passgen {
+            enab_letters: args.letters_on,
+            enab_u_letters: args.u_letters_on,
+            enab_num: args.nums_on,
+            enab_spec_symbs: args.spec_symbs_on,
+            enab_strong_usab: args.strong_usab_on,
+            custom_charset: args.custom_charset.leak(),
+        };
+        println!("{}", pgen.generate(args.length));
     }
 }
